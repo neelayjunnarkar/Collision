e9dfbe73ca7375dcc938fb8f909e766225f06fd1
TPackard <tyler.packard86@gmail.com>
2015-01-15 09:24:11 -0800

    Created classes

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * A pro physics simulator.
+ *
+ * @author Tyler Packard
+ * @author Neelay Junnarkar
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/physics/Vector.java
@@ -0,0 +1,13 @@
+package physics;
+
+/**
+ * A 2D Vector in polar form.
+ *
+ * @author Tyler Packard
+ */
+public class Vector {
+    double length = 0;
+    double angle = 0;
+
+
+}
